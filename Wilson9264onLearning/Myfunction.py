eaa5a16da2368b2455b46e1ac8dc8da097d596c6
Wilson Wang <39845364+Disciple9264@users.noreply.github.com>
2020-07-12 20:03:06 +0800

    Add files via upload

---
+++ Wilson9264onLearning/Myfunction.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 12:05:43 2020
+
+@author: Wilson
+"""
+
+def myabs(x):
+    if x>0:
+        return x
+    else:
+        return -x
+    
+def myabs1(x):
+    if x>0:
+        return x
+    else:
+        return -x
---
+++ Wilson9264onLearning/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 09:47:23 2020
+
+@author: Wilson
+"""
+from Myfunction import myabs 
+#a=input('please input a number:')
+#rint('hello, %s %d' % ('Yuchen',100))
+
+print(myabs(-1000))
